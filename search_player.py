2e8a2e1d4d5372b7e3edfe13db75d5188ad03395
sentry456123 <sentry456123@gmail.com>
2023-02-26 08:18:16 +0900

    changed

--- search_player.py
+++ search_player.py
@@ -2,6 +2,13 @@ import vlc
 import os
 import pygame
 import numpy
+import enum
+
+
+class Mode(enum.Enum):
+    DIR = 1
+    COMMAND = 2
+    ERROR = 3
 
 
 player: vlc.MediaPlayer = vlc.MediaPlayer()
@@ -9,9 +16,13 @@ current_files = os.listdir()
 selection = 0
 font: pygame.font.Font
 screen: pygame.Surface
-input_buffer = ""
+filename_buffer = ""
+command_buffer = ""
 starting_point = 0
 font_size = 40
+
+mode = Mode.DIR
+error_message = ""
 
 
 def search_string(text: str, pattern: str) -> bool:
@@ -30,18 +41,18 @@ def search_string(text: str, pattern: str) -> bool:
 
 def parent_dir():
     global current_files
-    global input_buffer
+    global filename_buffer
     global selection
 
     os.chdir('../')
     current_files = os.listdir()
-    input_buffer = ""
+    filename_buffer = ""
     selection = 0
 
 
 def open_file(index: int):
     global current_files
-    global input_buffer
+    global filename_buffer
     global selection
 
     if len(current_files) == 0:
@@ -51,7 +62,7 @@ def open_file(index: int):
     if os.path.isdir(next_path):
         os.chdir(next_path)
         current_files = os.listdir()
-        input_buffer = ""
+        filename_buffer = ""
         selection = 0
     else:
         player.set_mrl(next_path)
@@ -72,32 +83,124 @@ def down():
     selection = numpy.clip(selection, 0, len(current_files) - 1)
 
 
+def render_text(text: str, position, color=(255, 255, 255)):
+    font_image = font.render(text, True, color)
+    screen.blit(font_image, position)
+
+
+def render_dir():
+    pygame.draw.rect(screen, (225, 30, 30), (0, (selection + 1 + starting_point) * font_size, screen.get_width(), font_size))
+    for i, filename in enumerate(current_files):
+        if os.path.isdir(filename):
+            render_text(filename + "/", (0, (i + 1 + starting_point) * font_size), color=(255, 255, 0))
+        else:
+            render_text(filename, (0, (i + 1 + starting_point) * font_size))
+
+
+def render_filename_buffer():
+    pygame.draw.rect(screen, (30, 30, 225), (0, 0, screen.get_width(), font_size))
+    render_text(filename_buffer, (0, 0))
+
+
+def render_command_buffer():
+    pygame.draw.rect(screen, (30, 255, 30), (0, 0, screen.get_width(), font_size))
+    render_text(command_buffer, (0, 0))
+
+
+def render_error():
+    render_text(error_message, (0, 0))
+
+
+def exe_cmd(cmd: list[str]):
+    if len(cmd) == 0:
+        raise Exception("Nothing to execute")
+    match cmd[0].lower():
+        case "open" | "o":
+            match len(cmd):
+                case 1:
+                    open_file(selection)
+                case 2:
+                    try:
+                        open_file(int(cmd[1]))
+                    except:
+                        raise Exception(f"\"{cmd[1]}\" was expected as integer")
+                case _:
+                    raise Exception("Too many arguments")
+        case "stop":
+            player.stop()
+        case "play" | "s":
+            player.play()
+        case "pause" | "p":
+            player.pause()
+        case _:
+            raise Exception(f"There is no command named \"{cmd[0]}\"")
+
+
+def tokenize(raw_cmd: str) -> list[str]:
+    return raw_cmd.split()
+
+
 def on_key_down(key: int, mod: int, unicode: str):
     global selection
-    global input_buffer
+    global filename_buffer
+    global mode
+    global command_buffer
+    global error_message
 
-    if mod & pygame.KMOD_CTRL:
-        pass
-    else:
-        match key:
-            case pygame.K_ESCAPE:
-                parent_dir()
-            case pygame.K_RETURN:
-                open_file(selection)
-            case pygame.K_UP:
-                up()
-            case pygame.K_DOWN:
-                down()
-            case pygame.K_LEFT:
-                player.pause()
-            case pygame.K_RIGHT:
-                player.play()
-            case pygame.K_BACKSPACE:
-                input_buffer = input_buffer[:-1]
-                selection = 0
-            case _:
-                input_buffer += unicode
-                selection = 0
+    match mode:
+        case Mode.DIR:
+            if mod & pygame.KMOD_CTRL:
+                match key:
+                    case pygame.K_c:
+                        mode = Mode.COMMAND
+                    case pygame.K_d:
+                        filename_buffer = ""
+                    case pygame.K_p:
+                        parent_dir()
+                    case pygame.K_s:
+                        player.play()
+                    case pygame.K_q:
+                        player.pause()
+            else:
+                match key:
+                    case pygame.K_ESCAPE:
+                        pass
+                    case pygame.K_RETURN | pygame.K_RIGHT:
+                        open_file(selection)
+                    case pygame.K_UP:
+                        up()
+                    case pygame.K_DOWN:
+                        down()
+                    case pygame.K_LEFT:
+                        parent_dir()
+                    case pygame.K_BACKSPACE:
+                        filename_buffer = filename_buffer[:-1]
+                        selection = 0
+                    case _:
+                        filename_buffer += unicode
+                        selection = 0
+        case Mode.COMMAND:
+            if mod & pygame.KMOD_CTRL:
+                match key:
+                    case pygame.K_d:
+                        command_buffer = ""
+            else:
+                match key:
+                    case pygame.K_ESCAPE:
+                        mode = Mode.DIR
+                    case pygame.K_RETURN:
+                        try:
+                            exe_cmd(tokenize(command_buffer))
+                        except(Exception) as e:
+                            error_message = str(e)
+                            mode = Mode.ERROR
+                    case pygame.K_BACKSPACE:
+                        command_buffer = command_buffer[:-1]
+                    case _:
+                        command_buffer += unicode
+        case Mode.ERROR:
+            mode = Mode.DIR
+            error_message = ""
 
 
 def on_mouse_button_down(button: int):
@@ -105,38 +208,35 @@ def on_mouse_button_down(button: int):
     global font_size
     global font
 
-    match button:
-        case pygame.BUTTON_LEFT:
-            x, y = pygame.mouse.get_pos()
-            selection = int(y / font_size - starting_point - 1)
-            selection = numpy.clip(selection, 0, len(current_files) - 1)
-        case pygame.BUTTON_RIGHT:
-            open_file(selection)
-        case pygame.BUTTON_WHEELDOWN:
-            if pygame.key.get_mods() & pygame.KMOD_CTRL:
-                font_size -= 2
-                font_size = numpy.clip(font_size, 20, 100)
-                font = pygame.font.SysFont(None, font_size)
-            else:
-                down()
-        case pygame.BUTTON_WHEELUP:
-            if pygame.key.get_mods() & pygame.KMOD_CTRL:
-                font_size += 2
-                font_size = numpy.clip(font_size, 20, 100)
-                font = pygame.font.SysFont(None, font_size)
-            else:
-                up()
-        case pygame.BUTTON_MIDDLE:
-            parent_dir()
-        case pygame.BUTTON_X1:
-            player.pause()
-        case pygame.BUTTON_X2:
-            player.play()
-
-
-def render_text(text: str, position, color=(255, 255, 255)):
-    font_image = font.render(text, True, color)
-    screen.blit(font_image, position)
+    match mode:
+        case Mode.DIR:
+            match button:
+                case pygame.BUTTON_LEFT:
+                    x, y = pygame.mouse.get_pos()
+                    selection = int(y / font_size - starting_point - 1)
+                    selection = numpy.clip(selection, 0, len(current_files) - 1)
+                case pygame.BUTTON_RIGHT:
+                    open_file(selection)
+                case pygame.BUTTON_WHEELDOWN:
+                    if pygame.key.get_mods() & pygame.KMOD_CTRL:
+                        font_size -= 2
+                        font_size = numpy.clip(font_size, 20, 100)
+                        font = pygame.font.SysFont(None, font_size)
+                    else:
+                        down()
+                case pygame.BUTTON_WHEELUP:
+                    if pygame.key.get_mods() & pygame.KMOD_CTRL:
+                        font_size += 2
+                        font_size = numpy.clip(font_size, 20, 100)
+                        font = pygame.font.SysFont(None, font_size)
+                    else:
+                        up()
+                case pygame.BUTTON_MIDDLE:
+                    parent_dir()
+                case pygame.BUTTON_X1:
+                    player.pause()
+                case pygame.BUTTON_X2:
+                    player.play()
 
 
 def main():
@@ -144,7 +244,7 @@ def main():
     global selection
     global font
     global screen
-    global input_buffer
+    global filename_buffer
     global starting_point
 
     running = True
@@ -170,21 +270,24 @@ def main():
 
         current_files = []
         for file in os.listdir():
-            if search_string(file.lower(), input_buffer.lower()):
+            if search_string(file.lower(), filename_buffer.lower()):
                 current_files.append(file)
-
-        screen.fill((30, 30, 30))
 
         starting_point = 0
         if selection >= (screen.get_height() / font_size) / 2:
             starting_point = -selection + (screen.get_height() / font_size) / 2 - 1
-        
-        pygame.draw.rect(screen, (225, 30, 30), (0, (selection + 1 + starting_point) * font_size, screen.get_width(), font_size))
-        for i, filename in enumerate(current_files):
-            render_text(filename, (0, (i + 1 + starting_point) * font_size))
 
-        pygame.draw.rect(screen, (30, 30, 225), (0, 0, screen.get_width(), font_size))
-        render_text(input_buffer, (0, 0))
+        screen.fill((30, 30, 30))
+
+        match mode:
+            case Mode.DIR:
+                render_dir()
+                render_filename_buffer()
+            case Mode.COMMAND:
+                render_dir()
+                render_command_buffer()
+            case Mode.ERROR:
+                render_error()
 
         pygame.display.update()
 
